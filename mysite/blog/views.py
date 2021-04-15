ff5f980356089690426e088ef327334630b76d36
Nikolos123 <Isponec2>
2021-04-15 16:37:36 +0300

    added new file

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {'posts': posts,
+               'title': 'My blog'}
+    return render(request, 'blog/post/list.html', context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    context = {'post': post,
+               'title': 'My blog'}
+    return render(request, 'blog/post/detail.html', context)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog.views import post_list
 
 urlpatterns = [
+    path('', post_list, name="index"),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog'))
+
 ]
